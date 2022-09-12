267681e842861a84aa5b0716a1daed7962a6ed32
gnunesns <nunesguilherme920@gmail.com>
2022-09-12 16:50:15 -0300

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
+import BoxText from './components/BoxText'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
-import SelectLangue from './components/SelectLangue'
 
  export default function App() {
   return (
     <div className="App">
       <NavBar/>
-      <SelectLangue/>
-      <Footer/>
-      
+      <BoxText/>
+      <Footer/>  
     </div>
   )
 }
